bff9365ce6ccacce840a44a87b50cda3ef2d61dd
q100036q <184212460@qq.com>
2014-10-08 18:51:42 +0800

    'JavaScript练习'

---
+++ L08canvas/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by hp-dhr on 2014/10/8.
+ */
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+context.fillStyle="#ff0000";
+context.fillRect(0,0,100,100);
